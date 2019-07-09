df70356eb9c5386dea1ce59fcd64d2c2193feaf1
Viktor-al88 <viktor_al@rambler.ru>
2019-07-09 15:11:48 +0300

    создание BaseObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,11 @@
+
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
